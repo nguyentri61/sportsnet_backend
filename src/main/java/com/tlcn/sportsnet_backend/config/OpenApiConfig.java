122e8bcaf243d2f443852ed09bde23fab897db71
Danhuynh224 <vdan2242004@gmail.com>
2026-03-30 21:09:59 +0700

    add authen for swagger

--- src/main/java/com/tlcn/sportsnet_backend/config/OpenApiConfig.java
+++ src/main/java/com/tlcn/sportsnet_backend/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.tlcn.sportsnet_backend.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,7 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("SportsNet Backend API")
@@ -24,6 +28,14 @@ public class OpenApiConfig {
                                 .email("support@sportsnet.com")))
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Development server")
-                ));
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
